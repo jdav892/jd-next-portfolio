7d79fb72c217a1b30cb7c4b3bd6b90d921cb148c
jdav892 <jadavila9@gmail.com>
2025-01-13 18:52:37 -0500

    feat: creates data to be displayed in grid

---
+++ data/index.ts
@@ -0,0 +1,240 @@
+export const navItems = [
+    {name: "About", link: "#about"},
+    {name: "Projects", link: "#projects"},
+    {name: "Testimonials", link: "#testimonials"},
+    {name: "Contact", link: "#contact"},
+];
+
+export const gridItems = [
+    {
+        id: 1,
+        title: "I prioritize client collaboration, fostering open communication",
+        description: "",
+        className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-end",
+        img: "/b1.svg",
+        spareImg: "",
+    },
+    {
+        id: 2,
+        title: "I'm very flexible with time zone communications",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "w-full h-full",
+        titleClassName: "justify-start",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 3,
+        title: "My tech stack",
+        description: "I'm constantly learning",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        imgClassName: "",
+        titleClassName: "justify-center",
+        img: "",
+        spareImg: "",
+    },
+    {
+        id: 4,
+        title: "Solution oriented tech enthusiast",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-start",
+        img: "/grid.svg",
+        spareImg: "/b4.svg",
+    },
+    {
+        id: 5,
+        title: "Currently building a sports data API",
+        description: "Under the Hood",
+        className: "md:col-span-3 md:row-span-2",
+        imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+        titleClassName: "justify-end md:justify-starts lg:justify-center",
+        img: "/b5.svg",
+        spareImg: "/grid.svg",
+    },
+    {
+        id: 6,
+        title: "Do you want to collaborate?",
+        description: "",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+        imgClassName: "",
+        titleClassName: "justify-end md:max-w-full max-w-60 text-center",
+        img: "",
+        spareImg: "",
+    },
+];
+
+export const projects = [
+    {
+        id: 1,
+        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        des: "",
+        img: "",
+        iconLists: [],
+        link: "",
+    },
+     {
+        id: 2,
+        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        des: "",
+        img: "",
+        iconLists: [],
+        link: "",
+    },
+     {
+        id: 3,
+        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        des: "",
+        img: "",
+        iconLists: [],
+        link: "",
+    },
+     {
+        id: 4,
+        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        des: "",
+        img: "",
+        iconLists: [],
+        link: "",
+    },
+     {
+        id: 5,
+        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        des: "",
+        img: "",
+        iconLists: [],
+        link: "",
+    }, 
+    {
+        id: 6,
+        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+        des: "",
+        img: "",
+        iconLists: [],
+        link: "",
+    },
+];
+
+
+export const testimonials = [
+    {
+        quote:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        name: "Someone",
+        title: "Title"
+    },
+     {
+        quote:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        name: "Someone",
+        title: "Title"
+    },
+     {
+        quote:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        name: "Someone",
+        title: "Title"
+    },
+    {
+        quote:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        name: "Someone",
+        title: "Title"
+    },
+     {
+        quote:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        name: "Someone",
+        title: "Title"
+    },
+     {
+        quote:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        name: "Someone",
+        title: "Title"
+    },
+];
+
+export const companies = [
+    {
+        id: 1,
+        name: "cloudinary",
+        img: "/cloud.svg",
+        nameImg: "/cloudName.svg",
+    },  
+    {
+        id: 2,
+        name: "appwrite",
+        img: "/app.svg",
+        nameImg: "/app.svg",
+    }, 
+    {
+        id: 3,
+        name: "HOSTINGER",
+        img: "/host.svg",
+        nameImg: "/hostName.svg",
+    },
+    {
+        id: 4,
+        name: "stream",
+        img: "/s.svg",
+        nameImg: "/streamName.svg",
+    },
+    {
+        id: 5,
+        name: "docker",
+        img: "/dock.svg",
+        nameImg: "/dockerName.svg",
+    }, 
+
+];
+
+export const workExperience = [
+    {
+        id: 1,
+        title: "Frontend Developer",
+        desc: "description",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 2,
+        title: "Backend Engineer",
+        desc: "description",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 3,
+        title: "Web Designer",
+        desc: "description",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+    {
+        id: 4,
+        title: "Full-Stack Developer",
+        desc: "description",
+        className: "md:col-span-2",
+        thumbnail: "/exp1.svg",
+    },
+]
+
+export const socialMedia = [
+    {
+        id: 1,
+        img:"/git.svg",
+    },
+    {
+        id: 2,
+        img: "/twit.svg",
+    },
+    {
+        id: 3,
+        img: "/link.svg",
+    },
+];
