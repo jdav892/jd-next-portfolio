cd8757947b83f61164492a49d136ba1f1b0516d6
jdav892 <jadavila9@gmail.com>
2025-01-17 16:42:00 -0500

    feat: renders project components with pin down animations

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { projects } from '@/data'
 import { title } from 'process'
 import { img } from 'framer-motion/client'
+import { PinContainer } from './ui/3d-pin'
 
 const RecentProjects = () => {
   return (
@@ -13,7 +14,18 @@ const RecentProjects = () => {
         <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
             {projects.map(({ id, title, des, img, iconLists,link }) => (
                 <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
-                    {title}
+                    <PinContainer title={title} href={link}>
+                        <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh]'>
+                            <div>
+                                <img src="/bg.png" alt="bg-img" />
+                            </div>
+                            <img
+                                src={img}
+                                alt={title}
+                                className="z-10 absolute bottom-0"
+                            />
+                        </div>
+                    </PinContainer>
                 </div>
             ))}
         </div>
