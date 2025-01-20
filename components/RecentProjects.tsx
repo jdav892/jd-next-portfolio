a4e269cbac9934a5bc9869c8f02e3c03ac50672e
jdav892 <jadavila9@gmail.com>
2025-01-20 16:06:58 -0500

    feat: creates images of recent projects to be renderdd

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -13,14 +13,14 @@ const RecentProjects = () => {
             {projects.map(({ id, title, des, img, iconLists,link }) => (
                 <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
                     <PinContainer title={title} href={link}>
-                        <div className='relative flex items-center justify-center sm:w-96 w-[100vw] overflow-hidden h-[30vh]'>
-                            <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-black">
-                                <img src="/bg.png" alt="bg-img" />
+                        <div className='relative flex items-center justify-center sm:w-96 w-[100vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
+                            <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-black object-cover">
+                                <img src="/bg.png" alt="bgimg" />
                             </div>
                             <img
                                 src={img}
                                 alt=""
-                                className="z-10 absolute bottom-0"
+                                className="z-10 absolute bottom-0 object-fill h-full w-full"
                             />
                         </div>
                         <h1 className="font-bold lg:text-xl md:text-md text-base line-clamp-1">
