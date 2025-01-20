dc3d105a5a85e3622fe4a94a052c8d6519963849
jdav892 <jadavila9@gmail.com>
2025-01-20 16:24:18 -0500

    feat: creates a client section for testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Hero from "@/components/Hero"
 import { FloatingNav } from "@/components/ui/FloatingNav"
 import Grid from "@/components/Grid"
 import RecentProjects from "@/components/RecentProjects"
+import Clients from "@/components/Clients"
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <Hero/>
         <Grid />
         <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
