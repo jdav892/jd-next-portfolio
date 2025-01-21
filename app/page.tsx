3406a3ac6a2f0526d82453f35f8d9fd4ee154652
jdav892 <jadavila9@gmail.com>
2025-01-21 14:04:36 -0500

    feat: creates experience component

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { FloatingNav } from "@/components/ui/FloatingNav"
 import Grid from "@/components/Grid"
 import RecentProjects from "@/components/RecentProjects"
 import Clients from "@/components/Clients"
+import Experience from "@/components/Experience"
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );
